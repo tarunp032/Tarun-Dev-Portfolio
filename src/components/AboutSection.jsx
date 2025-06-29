ff45088dadd285579c39966cd41f0950ec2d426a
tarunp032 <prajapattarun7568@gmail.com>
2025-06-29 23:02:18 +0530

    🚀 Fresh full commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,5 @@
 import { Briefcase, Code, User } from "lucide-react";
+import resume from "../assets/resume.pdf";
 
 export const AboutSection = () => {
   return (
@@ -36,7 +37,8 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href={resume}
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
